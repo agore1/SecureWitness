5a6ec9c1802f55af7f00de5db26d44393a96dd57
Austin Gore <austin.gore@gmail.com>
2015-04-24 17:16:55 -0400

    Fixed simple backend urls.

--- stand_alone/main.py
+++ stand_alone/main.py
@@ -3,9 +3,16 @@ import click
 import requests
 from simplecrypt import encrypt, decrypt
 
+s = requests.Session()  # Session variable keeps cookies intact for authentication
+
+
 @click.group()
 def main():
-    click.echo('This is the standalone program.')
+    pass
+    # click.echo('This is the standalone program.')
+    # Program wide system variables for maintaining authentication
+
+
 
 
 @main.command()
@@ -13,15 +20,21 @@ def login():
     """Authenticate with the Secure Witness server."""
     username = click.prompt('Please enter your username', type=str)
     password = click.prompt('Please enter your password', hide_input=True, type=str)
-    click.echo('The username was: {0} and the password was: {1}'.format(username, password))
+    # click.echo('The username was: {0} and the password was: {1}'.format(username, password))
 
-    s = requests.Session()
-    s.get('http://127.0.0.1:8000/accounts/login/')
-    r = requests.get('http://127.0.0.1:8000/accounts/login/')
+    login_response = s.get('http://127.0.0.1:8000/accounts/login/')  # Obtain a csrf cookie
+    # click.echo(login_response.text)
+    # click.echo('\n The login cookie was: \n')
+    # click.echo(login_response.cookies['csrftoken'])
+    # Format form data for authentication
+    payload = {'password': password, 'username': username, 'csrfmiddlewaretoken': login_response.cookies['csrftoken']}
+    r = s.post('http://127.0.0.1:8000/accounts/login/', data=payload)
+    # r = s.get('http://127.0.0.1:8000/accounts/profile/')
+    # TODO: Check for login success
     click.echo(r.text)
-    # response = urllib.request.urlopen('http://127.0.0.1:8000/standalone/')
-    # html = response.read()
-    # click.echo(html)
+    click.echo("You are logged in now.")
+    click.echo(s.cookies)
+
 
 @main.command()
 def dec():
@@ -39,7 +52,7 @@ def dec():
 
 @main.command()
 def enc():
-    """Debug only: encrypt a file."""
+    """Encrypt a file."""
     filename = click.prompt('Please enter the filename to be encrypted', type=str)
     password = click.prompt('Please enter your password', hide_input=True, type=str)
     # Open the file and encrypt it
@@ -51,5 +64,10 @@ def enc():
     click.echo('Finished encrypting out file.')
 
 
-
+@main.command()
+def listreports():
+    """List all reports that are visible to the logged-in user"""
+    click.echo(s.cookies)
+    # r = s.get('http://127.0.0.1:8000/accounts/profile/')
+    # click.echo(r.text)
 
